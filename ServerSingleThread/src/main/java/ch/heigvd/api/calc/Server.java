74a8f6ae2adb550cec91c2983d23eb2d6ba1f0a0
Stefano <stefano.pontarolo@heig-vd.ch>
2021-11-09 21:23:32 +0100

    fix and finish both server

--- ServerSingleThread/src/main/java/ch/heigvd/api/calc/Server.java
+++ ServerSingleThread/src/main/java/ch/heigvd/api/calc/Server.java
@@ -16,7 +16,7 @@ public class Server {
     private final int TEST_DURATION = 5000;
     private final int PAUSE_DURATION = 1000;
     private final int NUMBER_OF_ITERATIONS = TEST_DURATION / PAUSE_DURATION;
-    private final int LISTEN_PORT = 9999;
+    private final int LISTEN_PORT = 2424;
 
     /**
      * Main function to start the server
@@ -31,24 +31,39 @@ public class Server {
     /**
      * Start the server on a listening socket.
      */
-    private void start() throws IOException {
-        /* TODO: implement the receptionist server here.
-         *  The receptionist just creates a server socket and accepts new client connections.
-         *  For a new client connection, the actual work is done by the handleClient method below.
-         */
+    private void start() {
+
         LOG.info("Starting server...");
 
         ServerSocket serverSocket;
-        Socket clientSocket;
+        Socket clientSocket = null;
 
-        LOG.log(Level.INFO, "Creating a server socket and binding it on any of the available network interfaces and on port {0}", new Object[]{Integer.toString(LISTEN_PORT)});
-        serverSocket = new ServerSocket(LISTEN_PORT);
-        clientSocket = serverSocket.accept();
+        try {
+            serverSocket = new ServerSocket(LISTEN_PORT);
+        } catch (IOException e) {
+            LOG.log(Level.SEVERE, e.toString(), e);
+            return;
+        }
 
-        handleClient(clientSocket);
+        while (true) {
+            try {
+                LOG.log(Level.INFO, "Waiting for client");
+                clientSocket = serverSocket.accept();
 
-        clientSocket.close();
-
+                handleClient(clientSocket);
+            } catch (IOException e) {
+                LOG.log(Level.SEVERE, e.toString(), e);
+                return;
+            } finally {
+                try {
+                    if (clientSocket != null && !clientSocket.isClosed()) {
+                        clientSocket.close();
+                    }
+                } catch (IOException e) {
+                    LOG.log(Level.SEVERE, e.toString(), e);
+                }
+            }
+        }
     }
 
     /**
@@ -56,81 +71,86 @@ public class Server {
      *
      * @param clientSocket with the connection with the individual client.
      */
-    private void handleClient(Socket clientSocket) throws IOException {
+    private void handleClient(Socket clientSocket) {
 
-        BufferedReader reader;
-        BufferedWriter writer;
+        BufferedReader reader = null;
+        BufferedWriter writer = null;
 
-        LOG.log(Level.INFO, "Getting a Reader and a Writer connected to the client socket...");
-        reader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream(), "UTF-8"));
-        writer = new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream(), "UTF-8"));
+        try {
+            LOG.log(Level.INFO, "Getting a Reader and a Writer connected to the client socket...");
+            reader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream(), "UTF-8"));
+            writer = new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream(), "UTF-8"));
 
-        LOG.log(Level.INFO, "Starting my job... sending current time to the client for {0} ms", TEST_DURATION);
-        writer.write("Vous pouvez effectuer les opérations suivantes +, -, *, /, %, ^ entre deux nombres.");
-        while (true) {
-            String input = reader.readLine();
-            if(input.isEmpty()){
-                writer.write("Vous pouvez effectuer les opérations suivantes +, -, *, /, %, ^ entre deux nombres.");
-                writer.flush();
+            LOG.log(Level.INFO, "Starting my job... sending current time to the client for {0} ms", TEST_DURATION);
+            writer.write("Vous pouvez effectuer les opérations suivantes +, -, *, /, %, ^ entre deux nombres.");
+            writer.flush();
+            while (true) {
+                String input = reader.readLine();
+                if (input.isEmpty()) {
+                    writer.write("Vous pouvez effectuer les opérations suivantes +, -, *, /, %, ^ entre deux nombres.");
+                    writer.flush();
+                }
+                if (input.equals("Bye")) {
+                    break;
+                }
+                input = input.trim();
+                String[] newInput = input.split("\\s+");
+                int a = Integer.parseInt(newInput[0]);
+                int b = Integer.parseInt(newInput[2]);
+                switch (newInput[1]) {
+                    case "+":
+                        writer.write(a + b);
+                        writer.flush();
+                        break;
+                    case "-":
+                        writer.write(a - b);
+                        writer.flush();
+                        break;
+                    case "*":
+                        writer.write(a * b);
+                        writer.flush();
+                        break;
+                    case "/":
+                        if (b != 0) {
+                            writer.write(a / b);
+
+                        } else {
+                            writer.write("Undefined");
+                        }
+                        writer.flush();
+                        break;
+                    case "%":
+                        if (b != 0) {
+                            writer.write(a % b);
+
+                        } else {
+                            writer.write("Undefined");
+                        }
+                        writer.flush();
+                        break;
+                    case "^":
+                        writer.write((int) Math.pow(a, b));
+                        writer.flush();
+                        break;
+                }
             }
-            if(input.equals("bye")){
-                break;
+        } catch (IOException e) {
+            LOG.log(Level.SEVERE, e.toString(), e);
+        } finally {
+            try {
+                if (reader != null) {
+                    reader.close();
+                }
+            } catch (IOException e) {
+                LOG.log(Level.SEVERE, e.toString(), e);
             }
-            input = input.trim();
-            String [] newInput = input.split("\\s+");
-            int a = Integer.parseInt(newInput[0]);
-            int b = Integer.parseInt(newInput[2]);
-            switch (newInput[1]){
-                case "+":
-                    writer.write(a + b);
-                    writer.flush();
-                    break;
-                case "-":
-                    writer.write(a - b);
-                    writer.flush();
-                    break;
-                case "*":
-                    writer.write(a * b);
-                    writer.flush();
-                    break;
-                case "/":
-                    if(b != 0){
-                        writer.write(a / b);
-
-                    }else{
-                        writer.write("Undefined");
-                    }
-                    writer.flush();
-                    break;
-                case "%":
-                    if(b != 0){
-                        writer.write(a % b);
-
-                    }else{
-                        writer.write("Undefined");
-                    }
-                    writer.flush();
-                    break;
-                case "^":
-                    writer.write((int) Math.pow(a,b));
-                    writer.flush();
-                    break;
+            try {
+                if (writer != null) {
+                    writer.close();
+                }
+            } catch (IOException e) {
+                LOG.log(Level.SEVERE, e.toString(), e);
             }
-
         }
-        writer.close();
-        reader.close();
-
-
-        /* TODO: implement the handling of a client connection according to the specification.
-         *   The server has to do the following:
-         *   - initialize the dialog according to the specification (for example send the list
-         *     of possible commands)
-         *   - In a loop:
-         *     - Read a message from the input stream (using BufferedReader.readLine)
-         *     - Handle the message
-         *     - Send to result to the client
-         */
-
     }
 }
